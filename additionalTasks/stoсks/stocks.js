5ab537e1f792efe035fa90152d2fd4f77cebe69d
Vlad Teterev <vladtett2000@gmail.com>
2020-11-02 20:57:07 +0300

    feat: added second additional task

--- additionalTasks/stoсks/stocks.js
+++ additionalTasks/stoсks/stocks.js
@@ -1,5 +1,13 @@
 function stocks(arr) {
-                                    
+    let result = 0;
+    let i = 0;
+    while (i < arr.length) {
+      if (arr[i + 1] > arr[i]) {
+        result += arr[i + 1] - arr[i];
+      }
+      i++;
+    }
+    return result;                         
 }
 
-console.log(stocks([7, 1, 5, 3, 6, 4]))
+console.log(stocks([7,6,4,3,1]))
