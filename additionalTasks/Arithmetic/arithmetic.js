adc4623eff1a83426e3714a914434dad8038d900
Vlad Teterev <vladtett2000@gmail.com>
2020-11-16 18:19:23 +0300

    initial commit

---
+++ additionalTasks/Arithmetic/arithmetic.js
@@ -0,0 +1,12 @@
+const add = (a, b) => a + b;
+
+const sub = (a, b) => a - b;
+
+const mul = (a, b) => a * b;
+
+const div = (a, b) => a / b;
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(mul(1, 2));
+console.log(div(1, 2));
